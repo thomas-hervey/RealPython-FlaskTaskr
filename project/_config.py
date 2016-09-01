5568e31a67d6f9e26f5e18f2186116468251ff8c
Thomas Hervey <thomasahervey@gmail.com>
2016-09-01 15:32:18 -0400

    added travs.yaml

--- project/_config.py
+++ project/_config.py
@@ -10,7 +10,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 CSRF_ENABLED = True
 SECRET_KEY = 'my_precious'
-DEBUG = False
+DEBUG = True
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
